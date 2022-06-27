b186c64256a8555c7e94610ac0f871f2cecf663e
Umar Aalam <ms1804101007@iiti.ac.in>
2022-06-27 19:36:19 +0530

    ch

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = express();
 app.use(express.static(__dirname + "/dist/social-networking-tool"));
 
 app.get("/*", (req, resp) => {
-  resp.sendFile(__dirname + "/dist/social-networking-tool/index.html");
+  resp.sendFile("/dist/social-networking-tool/index.html");
 });
 
 app.listen(process.env.PORT || 8080);
